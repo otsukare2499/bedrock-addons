5efbddad626341ff9d7e5acaedd38a4f63f85588
otsukare2499 <267944508+otsukare2499@users.noreply.github.com>
2026-03-29 18:57:26 +0900

    feat: make Vault Indestructible #26 (#29)

--- indestructible_blocks/behavior_pack/scripts/main.js
+++ indestructible_blocks/behavior_pack/scripts/main.js
@@ -2,7 +2,8 @@ import { world } from "@minecraft/server"
 
 const INDESTRUCTIBLE_BLOCK_TYPE_IDS = [
     "minecraft:mob_spawner",
-    "minecraft:trial_spawner"
+    "minecraft:trial_spawner",
+    "minecraft:vault"
 ]
 
 world.beforeEvents.playerBreakBlock.subscribe((event) => {
