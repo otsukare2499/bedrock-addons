2b3a26162317aacfac3be4f0604af6cf16707cf5
unknown <267944508+otsukare2499@users.noreply.github.com>
2026-04-13 16:59:32 +0900

    refactor: recognize repository structure #31

---
+++ behavior_pack/scripts/main.js
@@ -0,0 +1,26 @@
+import { world } from "@minecraft/server"
+
+const INDESTRUCTIBLE_BLOCK_TYPE_IDS = [
+    "minecraft:mob_spawner",
+    "minecraft:trial_spawner",
+    "minecraft:vault",
+    "minecraft:budding_amethyst"
+]
+
+world.beforeEvents.playerBreakBlock.subscribe((event) => {
+    const { block } = event;
+
+    if (INDESTRUCTIBLE_BLOCK_TYPE_IDS.includes(block.typeId)){
+        event.cancel = true;
+    }
+});
+
+world.beforeEvents.explosion.subscribe((event) => {
+    const impactedBlocks = event.getImpactedBlocks();
+
+    const filteredBlocks = impactedBlocks.filter(block =>
+        !INDESTRUCTIBLE_BLOCK_TYPE_IDS.includes(block.typeId)
+    );
+
+    event.setImpactedBlocks(filteredBlocks);
+});
